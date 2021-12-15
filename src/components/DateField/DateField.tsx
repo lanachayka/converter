bf991772d6202cc5936afa9ed459460594867d46
lanachayka <ruslana.chaikivska@gmail.com>
2021-12-15 13:35:31 +0200

    date component
    Signed-off-by: lanachayka <ruslana.chaikivska@gmail.com>

--- src/components/DateField/DateField.tsx
+++ src/components/DateField/DateField.tsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import st from './DateField.module.css'
 
 const DateField:React.FC = () => {
-    return <div className={st.wrapper}>Date will be here...</div>
+    const [clock, setClock] = useState<string>(new Date().toLocaleTimeString());
+
+    useEffect(()=> {
+        const clock = setInterval(
+            ():void => setClock(new Date().toLocaleTimeString()),
+            1000
+        );
+        return ()=> {
+            clearInterval(clock);
+        }
+    }, []);
+
+    return <div className={st.wrapper}>
+        <p className={st.text}>{new Date().toLocaleDateString(undefined, {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+        })}</p>
+        <span className={st.clock}>{clock}</span>
+    </div>
 }
 
 export default DateField
