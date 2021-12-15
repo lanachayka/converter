cac311a7858f3331aa1a868631a57efcbb654be3
lanachayka <ruslana.chaikivska@gmail.com>
2021-12-15 12:10:33 +0200

    update css files, add new component DateField
    Signed-off-by: lanachayka <ruslana.chaikivska@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import {Route, Routes } from 'react-router-dom';
 import Length from './components/Length/Length';
 import Weight from './components/Weight/Weight';
 import Temperature from './components/Temperature/Temperature';
+import DateField from "./components/DateField/DateField";
 
 const App:React.FC = () => {
   return (
     <div className="App">
+        <DateField />
         <Menu />
         <Routes>
             <Route path="/converter/length" element={<Length />}>Length</Route>
---
+++ src/components/DateField/DateField.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import st from './DateField.module.css'
+
+const DateField:React.FC = () => {
+    return <div className={st.wrapper}>Date will be here...</div>
+}
+
+export default DateField
